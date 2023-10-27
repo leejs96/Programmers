a1fb301906f8cf942a33ffd0095c2f3066fe58c7
leejs96 <134472265+leejs96@users.noreply.github.com>
2023-10-27 23:54:46 +0900

    문자열 정렬하기(2)

---
+++ LV.0/EX_120911/solution.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        String[] str = my_string.toLowerCase().split("");
+        
+        Arrays.sort(str);
+        for(String s : str) {
+            answer += s;
+        }
+        return answer;
+    }
+}
