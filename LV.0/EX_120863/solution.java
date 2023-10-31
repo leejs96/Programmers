77340b5d6315d9edb06f8a77205a4025cb7d360f
leejs96 <134472265+leejs96@users.noreply.github.com>
2023-10-31 21:14:56 +0900

    다항식 더하기

---
+++ LV.0/EX_120863/solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String solution(String polynomial) {
+        String answer = "";
+        String[] str = polynomial.split(" \\+ ");
+        
+        int a = 0;
+        int c = 0;
+        
+        for(String s : str) {
+           if(s.contains("x")) {
+               if(s.equals("x")) {
+                   a += 1;
+               } else {
+                   a += Integer.parseInt(s.substring(0, s.length()-1));
+               }
+           } else {
+               c += Integer.parseInt(s);
+           }
+        }
+        
+        if(a != 0) {
+            if(a != 1) {
+                answer += a + "x";
+            } else {
+                answer += "x";
+            }
+        }
+        
+        if(c != 0) {
+            if(a != 0) {
+                answer += " + " + c;
+            } else {
+                answer += Integer.toString(c);
+            }
+        }
+        
+        return answer;
+    }
+}
