ea1b28ae2610865cd0504c1523a5a38a5bf7ed6a
leejs96 <134472265+leejs96@users.noreply.github.com>
2023-10-31 21:30:03 +0900

    [Update] 최댓값 만들기(2)

--- LV.0/EX_120862/solution.java
+++ LV.0/EX_120862/solution.java
@@ -13,3 +13,19 @@ class Solution {
         return Collections.max(list);
     }
 }
+
+
+// 정렬 이용
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        
+        int f_max = numbers[0] * numbers[1];
+        int e_max = numbers[numbers.length-1] * numbers[numbers.length-2];
+        
+        int answer = Math.max(f_max, e_max);
+        return answer;
+    }
+}
