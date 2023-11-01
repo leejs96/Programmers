282cec33d3649f3030d600642902ab0411fbca22
leejs96 <134472265+leejs96@users.noreply.github.com>
2023-11-01 19:29:23 +0900

    Update 배열 비교하기

--- LV.0/EX_181856/solution.java
+++ LV.0/EX_181856/solution.java
@@ -3,8 +3,8 @@ class Solution {
         int answer = 0;
         int len1 = arr1.length;
         int len2 = arr2.length;
-        
-        if(len1 == len2) {
+        answer = Integer.compare(len1, len2);
+        if(answer == 0) {
             int sum1 = 0;
             int sum2 = 0;
             
@@ -15,15 +15,7 @@ class Solution {
             for(int i : arr2) {
                 sum2 += i;
             }
-            
-            if(sum1 == sum2) {
-                answer = 0;
-            } else {
-                answer = sum1 > sum2 ? 1 : -1;
-            }
-            
-        } else {
-            answer = len1 > len2 ? 1 : -1;
+            answer = Integer.compare(sum1, sum2);
         }
         
         return answer;
