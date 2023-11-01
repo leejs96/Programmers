8a6e45d588f4514abb679f1b49164aa7f915471b
leejs96 <134472265+leejs96@users.noreply.github.com>
2023-11-01 19:27:08 +0900

    Update 삼각형의 완성조건(2)

--- LV.0/EX_120868/solution.java
+++ LV.0/EX_120868/solution.java
@@ -4,11 +4,7 @@ class Solution {
         int min_side = Math.min(sides[0], sides[1]);
         int max_side = Math.max(sides[0], sides[1]);
         
-        for(int i = max_side - min_side; i < max_side; i++) {
-            answer++;
-        }
-        
-        for(int i = max_side + 1; i < max_side + min_side; i++) {
+        for(int i = max_side - min_side + 1; i < max_side + min_side; i++) {
             answer++;
         }
         
