58f0be3be76ba260a2253e788739d1fcddcd977c
leejs96 <134472265+leejs96@users.noreply.github.com>
2023-11-12 15:37:02 +0900

    Update 문자열 다루기 기본

--- LV.1/EX_12918/solution.java
+++ LV.1/EX_12918/solution.java
@@ -6,3 +6,22 @@ class Solution {
         return len == 4 || len == 6 ? Pattern.matches("^[0-9]*$", s) : false;
     }
 }
+
+
+// try-catch문 이용
+import java.util.regex.Pattern;
+
+class Solution {
+    public boolean solution(String s) {
+        int len = s.length();
+		
+		if(len == 4 || len == 6) {
+            try {
+            	return Pattern.matches("^[0-9]*$", s);
+            } catch(NumberFormatException e) {
+            	return false;
+            }
+        }
+        return false;
+    }
+}
