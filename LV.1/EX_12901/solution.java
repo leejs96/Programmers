acd0d93ca48d2c09bca1b9a94fbe5d92da4dd264
leejs96 <134472265+leejs96@users.noreply.github.com>
2023-11-15 12:40:52 +0900

    Update 2016년

--- LV.1/EX_12901/solution.java
+++ LV.1/EX_12901/solution.java
@@ -1,3 +1,4 @@
+// 1. LocalDate & DayOfWeek 클래스 사용
 import java.time.LocalDate;
 import java.time.DayOfWeek;
 
@@ -16,5 +17,30 @@ class Solution {
             case 6 : return "SAT";
             default : return "SUN";
         }
+    }
+}
+
+// 2.
+class Solution {
+    public String solution(int a, int b) {
+        int[] date = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int day = 0;
+        
+        for(int i = 0; i < a-1; i++) {
+            day += date[i];
+        }
+        
+        day = (day + b) % 7;
+        
+        switch(day) {
+            case 1 : return "FRI";
+            case 2 : return "SAT";
+            case 3 : return "SUN";
+            case 4 : return "MON";
+            case 5 : return "TUE";
+            case 6 : return "WED";
+            default: return "THU";
+        }
+        
     }
 }
