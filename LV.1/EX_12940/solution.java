6ad8b01ba8fc31bbfc9d435b80b82aada606121e
leejs96 <134472265+leejs96@users.noreply.github.com>
2023-12-05 13:08:58 +0900

    최대공약수와 최소공배수

---
+++ LV.1/EX_12940/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        
+        int max = Math.max(n, m);
+        int min = Math.min(n, m);
+        
+        for(int i = 1; i <= min; i++) {
+            if(max % i == 0 && min % i == 0) {
+                answer[0] = i;
+            }
+        }
+        
+        answer[1] = n * m / answer[0];
+        
+        return answer;
+    }
+}
