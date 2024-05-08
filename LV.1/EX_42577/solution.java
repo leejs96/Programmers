3318a2713eee7a2b689a3f16b7b77c1f9816c35c
leejs96 <134472265+leejs96@users.noreply.github.com>
2024-05-08 13:42:08 +0900

    전화번호 목록

---
+++ LV.1/EX_42577/solution.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean result = true;
+        Arrays.sort(phone_book);
+        
+        for(int i = 0; i < phone_book.length - 1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])) {
+                result = false;
+                break;
+            }
+        }
+        
+        return result;
+    }
+}
