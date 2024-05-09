248f1d0187961f23eb9148c91f614c66a0f97a39
leejs96 <134472265+leejs96@users.noreply.github.com>
2024-05-10 01:12:39 +0900

    가장 많이 받은 선물

--- LV.1/EX_258712/solution.java
+++ LV.1/EX_258712/solution.java
@@ -3,8 +3,8 @@ import java.util.HashMap;
 class Solution {
     public int solution(String[] friends, String[] gifts) {
         int answer = 0;
-        int giver = 0;
-        int taker = 0;
+        int giver = 0; // 선물 주는 사람 index
+        int taker = 0; // 선물 받는 사람 index
         int len = friends.length;
         
         int[][] matrix = new int[len][len]; // 주고 받은 선물 표시하는 행렬
@@ -18,22 +18,18 @@ class Solution {
         for(int i = 0; i < len; i++) { // 각 이름에 맞는 인덱스 저장
             friendIndex.put(friends[i], i);
         }
+    
         
-        // 주고 받은 선물
         for(int i=0; i < gifts.length; i++) {
             gift = gifts[i].split(" ");
             giver = friendIndex.get(gift[0]);
             taker = friendIndex.get(gift[1]);
             
-            matrix[giver][taker]++;
-        }
-        
-        // 선물 지수
-        for(int i = 0; i < len; i++) {
-            for(int j = 0; j < len; j++) {
-                giftIndex[i] += matrix[i][j];
-                giftIndex[i] -= matrix[j][i];
-            }
+            matrix[giver][taker]++; // 주고 받은 선물
+            
+            // 선물 지수
+            giftIndex[giver]++;
+            giftIndex[taker]--;
         }
         
         for(int i = 0; i < len-1; i++) {
